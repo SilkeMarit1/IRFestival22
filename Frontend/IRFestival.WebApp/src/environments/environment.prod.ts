4be7b26f0119b838a50bdb745eb0075700ab9735
Silke Marit <silke.marit@inetum-realdolmen.com>
2022-10-17 14:07:21 +0200

    apibaseuri

--- Frontend/IRFestival.WebApp/src/environments/environment.prod.ts
+++ Frontend/IRFestival.WebApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   festivalName: "IRFestival",
-  apiBaseUrl: "https://irfestivalapimsm.azure-api.net/2022/v1/api",
+  apiBaseUrl: "https://irfestivalapimsm.azure-api.net/2022/v1/api/",
   connectionstringAppInsights: "InstrumentationKey=f277355a-9671-4ac5-bb28-6a1c03527a4c;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/"
 };
